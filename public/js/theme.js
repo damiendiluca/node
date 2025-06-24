9eec91a302b4311c38b3ab459e016b84edec553e
cybertechangel <nelnihadnel@gmail.com>
2025-06-24 14:11:17 +0200

    Modification

--- public/js/theme.js
+++ public/js/theme.js
@@ -25,13 +25,24 @@ function setTheme(isDark) {
     updateIcon(isDark);
 }
 
+
+
 toggleBtn.addEventListener("click", () => {
+    // On vérifie si le corps de la page contient la classe "dark-mode", cela permet de savoir si le thème sombre est actuellement activé
     const isCurrentlyDark = document.body.classList.contains("dark-mode");
+
+    // On appelle setTheme avec l'inverse du thème actuel pour basculer (toggle) si le thème est sombre, on passe au clair, sinon on passe au sombre
     setTheme(!isCurrentlyDark);
 });
 
+// Quand la page HTML est complètement chargée (DOM)
 window.addEventListener("DOMContentLoaded", () => {
+    // On récupère la valeur enregistrée dans le localStorage sous la clé "theme"
     const savedTheme = localStorage.getItem("theme");
+
+    // On crée un booléen isDark : il vaut true si le thème sauvegardé est "dark"
     const isDark = savedTheme === "dark";
+
+    // On applique le thème correspondant (dark ou light) dès le chargement de la page
     setTheme(isDark);
 });
