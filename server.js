4e79cfe11b881ce98f93445542cb1511398a2d4b
Damien Di Luca <damiendiluca@MacBook-Air-de-Damien.local>
2025-06-24 14:09:04 +0200

    Server.js

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
-// Importer le module NATIF http pour créer un serveur web
-
 const http = require('http')
-
-// Importer le module FS (File system) pour nous permettre de lire ou créer des fichiers
-
 const fs = require('fs')
-
-// Importer le module path pour construire des chemins
-
 const path = require ('path')
 
 const server = http.createServer((req, res) => {
-    // Initier le chemin du fichier à server selon la route demandée
-    let filePath = './views' // Le dossier qui va contenir nos vues
-    let status = 200 // On suppose que tout est ok
-    let contentType = 'text/html' // On sert uniquement de l'HTML
+    let filePath = './public'
+    let status = 200
+    let contentType = 'text/html'
     
-
-    // On va regarder quelle URL est demandée
     switch (req.url) {
-        case './':
-            filePath += '/index.html' // Si je demande '/', je reçois index.html (Page d'accueil)
+        case '/':
+            filePath += '/index.html'
             break;
-            case './':
-            filePath += '/about.html' // Si je demande '/', je reçois index.html (Page d'accueil)
+
+            case '/about.html':
+            filePath += '/about.html'
             break;
-            case './':
-            filePath += '/contact.html' // Si je demande '/', je reçois index.html (Page d'accueil)
+
+            case '/contact.html':
+            filePath += '/contact.html'
             break;
+
             default:
             filePath += '/404.html'
             break;
